3647e1d5ee5a9750f039c8967a8dae05269ab971
Theresa Adam <theresa.adam@daimler.com>
2021-10-18 14:31:41 +0200

    basic functionality and styling

--- js/app.js
+++ js/app.js
@@ -22,7 +22,14 @@
  * Define Global Variables
  * 
 */
-
+// first step: put all 4 sections into a static NodeList, one section === one element 
+const sections = document.querySelectorAll('section');
+// loop over this NodeList like normal Array -> e.g. foreach()-loop
+// to create an unordered list of elements
+// use the #navbar__list id as it is already available in the HTML
+const navList = document.querySelector('#navbar__list');
+// using a document fragment improves the performance as reflow and repaint is only started once
+const navListFragment = document.createDocumentFragment();
 
 /**
  * End Global Variables
@@ -30,7 +37,35 @@
  * 
 */
 
+// to apply or remove some specific styling use the following helper functions add and remove
+// add function
+const addStyles = (item) => {
+  item.classList.add('activeSection');
+  const navClass = document.querySelector(`.${item.id}`);
+  navClass.classList.add('activeNavBar')
+}
 
+// remove function
+const removeStyles = (item) => {
+  item.classList.remove('activeSection');
+  const navClass = document.querySelector(`.${item.id}`);
+  navClass.classList.remove('activeNavBar')
+}
+
+// to check if section is near to viewport
+const changeToActive = (items) => {
+  for (const item of items) {
+    const box = item.getBoundingClientRect();
+    // played with the values of top and bottom and experienced the best result with the value of 250 
+    if (box.top <= 250 && box.bottom >= 250) {
+      // Apply active state on the current item and the corresponding Nav link.
+      addStyles(item);
+    } else {
+      // Remove active state from other item and corresponding Nav link.
+      removeStyles(item);
+    }
+  }
+}
 
 /**
  * End Helper Functions
@@ -39,13 +74,31 @@
 */
 
 // build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
+const navBar = (items) => {
+  items.forEach(function(item) {
+    // create single list element
+    const navListItem = document.createElement('li');
+    // set name of item and using href to link the correct item 
+    navListItem.innerHTML= `<a class='menu__link ${item.id}' href=#${item.id}>${item.dataset.nav}</a>`
+    // add elements to the fragment
+    navListFragment.appendChild(navListItem);
+    console.log(`successful for ${navListItem}`)
+  });
+  // add the list to the navbar
+  navList.appendChild(navListFragment);
+  // navigation bar was successfully added
+}
 
 // Scroll to anchor ID using scrollTO event
-
+const scrollToSection = (event) => {
+  event.preventDefault();
+  const href = event.target.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+  scrollTo({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
 
 /**
  * End Main Functions
@@ -54,9 +107,17 @@
 */
 
 // Build menu 
+navBar(sections);
 
 // Scroll to section on link click
+// add eventListener when clicking on the Navigation bar -> scroll to linked section
+navList.addEventListener('click', function(event) {
+  scrollToSection(event);
+});
 
 // Set sections as active
-
+// Add class 'active' to section when near top of viewport
+document.addEventListener("scroll", function() {
+  changeToActive(sections);
+});
 
